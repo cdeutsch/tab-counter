f9c9f098b1ab7d6d64518422425a6a234e5c08e0
Christopher Deutsch <cd@cdeutsch.com>
2021-08-31 09:59:26 -0500

    Get tab counting working.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
+import '@jxa/global-type';
 import clear from 'clear';
 import { program } from 'commander';
 import figlet from 'figlet';
 import { magenta } from 'kleur';
 import PrettyError from 'pretty-error';
+import { run } from '@jxa/run';
 
 const pe = new PrettyError();
 
@@ -12,9 +14,9 @@ async function main() {
   try {
     clear();
 
-    console.log(magenta(figlet.textSync('snipe', { horizontalLayout: 'full' })));
+    console.log(magenta(figlet.textSync('Tab Counts', { horizontalLayout: 'full' })));
 
-    program.version('0.0.1').description('Monitor a url for changes').parse(process.argv);
+    program.version('0.0.1').description('Send browser tab counts to Axiom').parse(process.argv);
 
     // if (!process.argv.slice(2).length) {
     //   program.outputHelp();
@@ -22,7 +24,35 @@ async function main() {
     //   return;
     // }
 
-    console.log('HI');
+    const result = await run(() => {
+      const Chrome = Application('/Applications/Google Chrome.app');
+
+      let tabCounts = [];
+
+      if (Chrome.running()) {
+        for (let ii in Chrome.windows) {
+          tabCounts.push(Chrome.windows[ii].tabs.length);
+        }
+
+        tabCounts.sort((a, b) => b - a);
+      }
+
+      const tabCount = tabCounts.reduce((accum, count) => {
+        return accum + count;
+      }, 0);
+
+      const data = {
+        tabCount: tabCount,
+        windowCount: tabCounts.length,
+        tabCounts: tabCounts,
+      };
+
+      console.log(JSON.stringify(data));
+
+      return data;
+    });
+
+    console.log('HI', JSON.stringify(result));
   } catch (e) {
     console.error(pe.render(e));
   }
