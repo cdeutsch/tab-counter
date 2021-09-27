48659039bd4cc5a8405a410f43e83d4aa756d9f9
Christopher Deutsch <cd@cdeutsch.com>
2021-09-27 09:03:42 -0500

    Better error handling to not crash if the osx script fails.

--- src/index.ts
+++ src/index.ts
@@ -26,80 +26,80 @@ function myParseInt(value, dummyPrevious) {
 }
 
 async function main() {
-  try {
-    console.log(magenta(figlet.textSync('Tab Counts', { horizontalLayout: 'full' })));
+  console.log(magenta(figlet.textSync('Tab Counts', { horizontalLayout: 'full' })));
 
-    program
-      .version('1.0.1')
-      .description('Send browser tab counts to Axiom')
-      .option('-i, --interval <number>', 'Execute snipes every X minutes', myParseInt, 5)
-      .option('-S, --schedule', 'Run infinitely using interval to re-run')
-      .parse(process.argv);
+  program
+    .version('1.0.1')
+    .description('Send browser tab counts to Axiom')
+    .option('-i, --interval <number>', 'Execute snipes every X minutes', myParseInt, 5)
+    .option('-S, --schedule', 'Run infinitely using interval to re-run')
+    .parse(process.argv);
 
-    // if (!process.argv.slice(2).length) {
-    //   program.outputHelp();
+  // if (!process.argv.slice(2).length) {
+  //   program.outputHelp();
 
-    //   return;
-    // }
+  //   return;
+  // }
 
-    const options = program.opts();
-    const intervalMinutes: number = options.interval;
+  const options = program.opts();
+  const intervalMinutes: number = options.interval;
 
-    async function run() {
-      console.log(magenta(formatDate()), 'Counting...\n');
+  async function run() {
+    console.log(magenta(formatDate()), 'Counting...\n');
 
-      const browserTabCounts: BrowserTabCounts[] = [];
-      const sampleTime = Date.now();
+    const browserTabCounts: BrowserTabCounts[] = [];
+    const sampleTime = Date.now();
 
-      for (const browser of SUPPORTED_BROWSERS) {
-        const tabCounts = await countTabs(browser, sampleTime);
-        if (tabCounts) {
-          browserTabCounts.push(tabCounts);
+    for (const browser of SUPPORTED_BROWSERS) {
+      const tabCounts = await countTabs(browser, sampleTime);
+      if (tabCounts) {
+        browserTabCounts.push(tabCounts);
+      }
+    }
+
+    console.log('🚀️', JSON.stringify(browserTabCounts));
+
+    const totalTabs = browserTabCounts.reduce((accum, browserTabCount) => {
+      return accum + browserTabCount.tabCount;
+    }, 0);
+
+    const totalWindows = browserTabCounts.reduce((accum, browserTabCount) => {
+      return accum + browserTabCount.windowCount;
+    }, 0);
+
+    console.log(magenta('\nΣ'), 'tabs:', totalTabs, ' windows:', totalWindows);
+
+    // return;
+
+    try {
+      // Send to Axiom.
+      await axios.post(
+        `https://cloud.dev.axiomtestlabs.co/api/v1/datasets/${process.env.AXIOM_DATASET}/ingest`,
+        browserTabCounts,
+        {
+          headers: { Authorization: `Bearer ${process.env.AXIOM_INGEST_TOKEN}` },
         }
-      }
-
-      console.log('🚀️', JSON.stringify(browserTabCounts));
-
-      const totalTabs = browserTabCounts.reduce((accum, browserTabCount) => {
-        return accum + browserTabCount.tabCount;
-      }, 0);
-
-      const totalWindows = browserTabCounts.reduce((accum, browserTabCount) => {
-        return accum + browserTabCount.windowCount;
-      }, 0);
-
-      console.log(magenta('\nΣ'), 'tabs:', totalTabs, ' windows:', totalWindows);
-
-      // return;
-
-      try {
-        // Send to Axiom.
-        await axios.post(
-          `https://cloud.dev.axiomtestlabs.co/api/v1/datasets/${process.env.AXIOM_DATASET}/ingest`,
-          browserTabCounts,
-          {
-            headers: { Authorization: `Bearer ${process.env.AXIOM_INGEST_TOKEN}` },
-          }
-        );
-      } catch (err) {
-        console.warn(yellow('\nFailed to send data to Axiom'), err);
-      }
+      );
+    } catch (err) {
+      console.warn(yellow('\nFailed to send data to Axiom'), err);
     }
-
-    if (options.schedule) {
-      // Do this every X minutes.
-      // eslint-disable-next-line no-constant-condition
-      while (true) {
-        await run();
-        console.log('\n⏳️\n');
-        await delay(intervalMinutes * 60000); // Convert minutes to milliseconds.
-      }
-    }
-
-    await run();
-  } catch (e) {
-    console.error(e);
   }
+
+  if (options.schedule) {
+    // Do this every X minutes.
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      try {
+        await run();
+      } catch (e) {
+        console.error(e);
+      }
+      console.log('\n⏳️\n');
+      await delay(intervalMinutes * 60000); // Convert minutes to milliseconds.
+    }
+  }
+
+  await run();
 }
 
 clear();
